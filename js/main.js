91d2f5b4555d9fcaea7f167facf5712306659084
WMI <chris@webmobileimage.com>
2015-01-02 10:12:51 -0500

    window resize var

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($) {
- 
+ 	
+ 	var $window = $(window);
+	var $winWidth = $(window).width();
+	$window.on('resize', function(){
+		$winWidth = $(window).width();
+	});
+
     Tabs();
     Modals();
     Tooltips();
