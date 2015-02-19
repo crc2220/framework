a405e6731417dd4867c66f7960e3ee8bd8b8c538
Chris Carey <crc2220@gmail.com>
2015-02-19 08:51:07 -0500

    mac or pc

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 jQuery(document).ready(function($) {
  	
+ 	// get window and window height(on resize)
  	var $window = $(window);
 	var $winWidth = $(window).width();
 	$window.on('resize', function(){
 		$winWidth = $(window).width();
 		console.log($winWidth);
 	});
-
 	console.log($winWidth);
+
+	// test if mac or pc
+	if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+	  $("body").addClass("mac");
+	} else {
+	  $("body").addClass("pc");
+	}
+
     Tabs();
     Modals();
     Tooltips();
